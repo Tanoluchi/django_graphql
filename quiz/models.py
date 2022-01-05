bf2dc3e526854613b8994c9d14fcdba3991f441a
Tanoluchi <ejemplolucianox15867@gmail.com>
2022-01-05 00:38:11 -0300

    second commit

---
+++ quiz/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Quizzes(models.Model):
+    title = models.CharField(max_length=255, default=_("New Quiz"))
+    category = models.ForeignKey(Category, default=1, on_delete=models.DO_NOTHING)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+
+
+class Answer(models.Model):
+    
